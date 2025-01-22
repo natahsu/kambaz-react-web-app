81035a5358f37000384dd8991661369819d747fe
natahsu <hsu.nata@northeastern.edu>
2025-01-22 16:32:25 -0500

    introducing headings

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -1,4 +1,10 @@
 export default function Lab1() {
-    return <h2>Lab 1</h2>;
+    return (
+        <div>
+            <h2>Lab 1</h2>
+            <h3>Headings</h3>
+            <p>Headings allow you to break up the content into multiple sections</p>
+        </div>>
+    )
     
 }
