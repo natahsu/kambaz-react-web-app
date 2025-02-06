c8bc543e917e515db9e9972204336e0c28b60ba9
natahsu <hsu.nata@northeastern.edu>
2025-02-05 23:26:20 -0500

    a2 CSS and Bootstrap

--- src/Kambaz/Navigation.tsx
+++ src/Kambaz/Navigation.tsx
@@ -1,32 +1,80 @@
-import { Link } from "react-router-dom";
+import { AiOutlineDashboard } from "react-icons/ai";
+import { IoCalendarOutline } from "react-icons/io5";
+import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
+import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
+import { Link } from "react-router";
 
 export default function KambazNavigation() {
-    return (
-        <div id="wd-kambaz-navigation">
-            <ul>
-                <li>
-                    <a href="https://www.northeastern.edu/" id="wd-neu-link" target="_blank">Northeastern</a>
-                </li>
-                <li>
-                    <Link to="/Kambaz/Account" id="wd-account-link">Account</Link>
-                </li>
-                <li>
-                    <Link to="/Kambaz/Dashboard" id="wd-dashboard-link">Dashboard</Link>
-                </li>
-                <li>
-                    <Link to="/Kambaz/Dashboard" id="wd-course-link">Courses</Link>
-                </li>
-                <li>
-                    <Link to="/Kambaz/Calendar" id="wd-calendar-link">Calendar</Link>
-                </li>
-                <li>
-                    <Link to="/Kambaz/Inbox" id="wd-inbox-link">Inbox</Link>
-                </li>
-                <li>
-                    <Link to="/Labs" id="wd-labs-link">Labs</Link>
-                </li>
-            </ul>
-        </div>
-    );
+  return (
+    <div 
+      id="wd-kambaz-navigation" 
+      style={{ width: 120 }}
+      className="list-group rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2"
+    >
+      <a 
+        id="wd-neu-link" 
+        target="_blank"
+        href="https://www.northeastern.edu/"
+        className="list-group-item bg-black border-0 text-center"
+      >
+        <img src="NEU.png" width="75px" />
+      </a>
+      
+      <Link 
+        to="/Kambaz/Account" 
+        id="wd-account-link"
+        className="list-group-item text-center border-0 bg-black text-white"
+      >
+        <FaRegCircleUser className="fs-1 text text-white" /><br />
+        Account
+      </Link>
+      
+      <Link 
+        to="/Kambaz/Dashboard" 
+        id="wd-dashboard-link"
+        className="list-group-item text-center border-0 bg-white text-danger"
+      >
+        <AiOutlineDashboard className="fs-1 text-danger" /><br />
+        Dashboard
+      </Link>
+      
+      <Link 
+        to="/Kambaz/Courses" 
+        id="wd-course-link"
+        className="list-group-item text-white bg-black text-center border-0"
+      >
+        <LiaBookSolid className="fs-1 text-white" /><br />
+        Courses
+      </Link>
+      
+      <Link 
+        to="/Kambaz/Calendar" 
+        id="wd-calendar-link"
+        className="list-group-item text-white bg-black text-center border-0"
+      >
+        <IoCalendarOutline className="fs-1 text-white" /><br />
+        Calendar
+      </Link>
+      
+      <Link 
+        to="/Kambaz/Inbox" 
+        id="wd-inbox-link"
+        className="list-group-item text-white bg-black text-center border-0"
+      >
+        <FaInbox className="fs-1 text-white" /><br />
+        Inbox
+      </Link>
+      
+      <Link 
+        to="/Kambaz/Settings" 
+        id="wd-settings-link"
+        className="list-group-item text-white bg-black text-center border-0"
+      >
+        <LiaCogSolid className="fs-1 text-white" /><br />
+        Settings
+      </Link>
+    </div>
+  );
 }
+
 
