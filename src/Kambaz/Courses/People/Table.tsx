87207e7f2593958978fa3fa34daeacb160532b04
natahsu <hsu.nata@northeastern.edu>
2025-02-19 23:41:17 -0500

    a3 javascript

--- src/Kambaz/Courses/People/Table.tsx
+++ src/Kambaz/Courses/People/Table.tsx
@@ -1,11 +1,37 @@
 import { useParams } from "react-router-dom";
-import db from "../../Database";
 import { Table } from "react-bootstrap";
 import { FaUserCircle } from "react-icons/fa";
+import users from "../../Database/users.json";
+import enrollments from "../../Database/enrollments.json";
+
+interface User {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  loginId: string;
+  section: string;
+  role: string;
+  lastActivity: string;
+  totalActivity: string;
+}
+
+interface Enrollment {
+  _id: string;
+  user: string;
+  course: string;
+}
 
 export default function PeopleTable() {
   const { cid } = useParams();
-  const { users, enrollments } = db;
+  console.log("Current course ID:", cid);
+
+  const filteredUsers = users.filter((user: User) =>
+    enrollments.some(
+      (enrollment: Enrollment) => enrollment.user === user._id && enrollment.course === cid
+    )
+  );
+
+  console.log("Filtered users:", filteredUsers);
 
   return (
     <div id="wd-people-table">
@@ -21,36 +47,20 @@ export default function PeopleTable() {
           </tr>
         </thead>
         <tbody>
-          {users
-            .filter((usr) =>
-              enrollments.some(
-                (enrollment) => 
-                  enrollment.user === usr._id && 
-                  enrollment.course === cid
-              )
-            )
-            .map((user) => (
-              <tr key={user._id}>
-                <td className="wd-full-name text-nowrap">
-                  <FaUserCircle className="me-2 fs-1 text-secondary" />
-                  <span className="wd-first-name">
-                    {user.firstName}
-                  </span>{" "}
-                  <span className="wd-last-name">
-                    {user.lastName}
-                  </span>
-                </td>
-                <td className="wd-login-id">{user.loginId}</td>
-                <td className="wd-section">{user.section}</td>
-                <td className="wd-role">{user.role}</td>
-                <td className="wd-last-activity">
-                  {user.lastActivity}
-                </td>
-                <td className="wd-total-activity">
-                  {user.totalActivity}
-                </td>
-              </tr>
-            ))}
+          {filteredUsers.map((user: User) => (
+            <tr key={user._id}>
+              <td className="wd-full-name text-nowrap">
+                <FaUserCircle className="me-2 fs-1 text-secondary" />
+                <span className="wd-first-name">{user.firstName}</span>{" "}
+                <span className="wd-last-name">{user.lastName}</span>
+              </td>
+              <td className="wd-login-id">{user.loginId}</td>
+              <td className="wd-section">{user.section}</td>
+              <td className="wd-role">{user.role}</td>
+              <td className="wd-last-activity">{user.lastActivity}</td>
+              <td className="wd-total-activity">{user.totalActivity}</td>
+            </tr>
+          ))}
         </tbody>
       </Table>
     </div>
