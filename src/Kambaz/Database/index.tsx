4fe64dd491f6baf7b8bfea1d847d7ce32b99a459
natahsu <hsu.nata@northeastern.edu>
2025-02-19 23:37:56 -0500

    a3

--- src/Kambaz/Database/index.tsx
+++ src/Kambaz/Database/index.tsx
@@ -2,6 +2,7 @@ import courses from "./courses.json";
 import modules from "./modules.json";
 import assignments from "./assignments.json";
 import users from "./users.json";
+import enrollments from "./enrollments.json";
 
-const db = { courses, modules, assignments, users };
+const db = { courses, modules, assignments, users, enrollments };
 export default db;
