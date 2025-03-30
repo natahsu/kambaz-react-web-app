74fb538d47e8d6f9d98108797f26ac5b94897d16
natahsu <hsu.nata@northeastern.edu>
2025-03-30 18:18:38 -0400

    a4 fix

--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -15,7 +15,7 @@ export default function Kambaz() {
           <Route path="/" element={<Navigate to="Account" />} />
           <Route path="/Account/*" element={<Account />} />
           <Route path="/Dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-          <Route path="/Courses/:cid/*" element={<ProtectedRoute><Courses /></ProtectedRoute>} />
+          <Route path="Courses/:cid/*" element={<ProtectedRoute><Courses courses={[]} /></ProtectedRoute>} />
           <Route path="/Calendar" element={<h1>Calendar</h1>} />
           <Route path="/Inbox" element={<h1>Inbox</h1>} />
         </Routes>
